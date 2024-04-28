4397f63a581fea4245aa1a53326f1b19a99453c9
Sandra Raj <sr6535@rit.edu>
2024-04-28 01:00:51 -0400

    sessions updated

--- businessLogic.js
+++ businessLogic.js
@@ -1,37 +1,3 @@
-// const bcrypt = require('bcrypt');
-// const { getUserByEmail } = require('./dataAccess');
-
-// async function loginUser(email, password) {
-//     try {
-//         const userData = await getUserByEmail(email);
-//         if (!userData) {
-//             return { success: false, message: 'Invalid credentials' };
-//         }
-
-//         const hashedPassword = userData.password;
-//         const usertype = userData.usertype;
-
-//         return new Promise((resolve, reject) => {
-//             bcrypt.compare(password, hashedPassword, function(err, result) {
-//                 if (err) {
-//                     reject('An error occurred while comparing passwords.');
-//                 }
-//                 if (result) {
-//                     // Redirect based on user type
-//                     let redirectUrl = usertype === 'officer' || usertype === 'member' ? '/society' : '/';
-//                     resolve({ success: true, message: 'Login successful', redirectUrl: redirectUrl });
-//                 } else {
-//                     resolve({ success: false, message: 'Invalid credentials' });
-//                 }
-//             });
-//         });
-//     } catch (error) {
-//         throw error;
-//     }
-// }
-
-// module.exports = { loginUser };
-
 const bcrypt = require('bcrypt');
 const { getUserByEmail } = require('./dataAccess');
 
